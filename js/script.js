8fd069b8c434f34121efa0405603ee9c7075e506
Koder-Koj <kojoann01@gmail.com>
2021-06-20 10:44:23 -0400

    Updated All Files Repo

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+
+'use strict';
+
+let btn = document.querySelector('.btn');
+
+
+btn.addEventListener('click', function () {
+    const loan = document.querySelector('#loan').value;
+
+
+    // Interest Calculation:
+    const interest = loan * 0.03;
+    document.querySelector('#interest').value = interest;
+
+    // Mortgage Payment:
+    const amortization = document.querySelector('#amortization').value;
+    const frequency = document.querySelector('#frequency').value;
+    const frequencyOption = document.querySelector('.frequency_monthly');
+    const frequencyCalc = function () {
+        if (frequency == 'monthly') {
+            frequency = '';
+            frequency = '12';
+            frequencyOption.removeAttribute('value', 'monthly');
+            frequencyOption.setAttribute('value', '12');
+        };  
+        console.log(typeof (frequencyCalc));
+
+    };
+
+
+    const time = Number(amortization) * Number(frequencyCalc());
+    console.log(typeof (time));
+    console.log(time);
+    const rate = 0.03;
+
+
+    const mortgage = function () {
+
+        const top = rate * ((1 + rate) ** time);
+        const bottom = ((1 + rate) ** time) - 1;
+        return loan * (Number(top) / Number(bottom));
+    }
+
+    console.log(typeof (mortgage()));
+
+    const mortgagePayment = mortgage();
+
+    document.querySelector('#payment').value = mortgagePayment.toFixed(2);
+})
+
+
